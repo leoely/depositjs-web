da8f9e556a03e94c728c0b174fb055e077d87494
Leo Brad <>
2024-04-30 15:06:13 +0800

    [Change] Change update check time.

--- src/client/script/component/WebApp/index.js
+++ src/client/script/component/WebApp/index.js
@@ -14,7 +14,7 @@ class WebApp extends React.Component {
   }
 
   dealUpdate() {
-    const id = setInterval(this.checkUpdate, 1000 * 1);
+    const id = setInterval(this.checkUpdate, 1000 * 60 * 30);
     this.id = id;
   }
 
